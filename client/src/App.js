1c2b5ca6b9b26f9d66030a5e87aef556eea983cc
Donda <34602885+dondasasmita@users.noreply.github.com>
2022-03-04 13:22:59 +0800

    Add clock component to the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
 import { render } from 'react-dom';
 // import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Form from './components/Form';
+import Clock from './components/Clock';
 // import ErrorBoundary from './ErrorBoundary';
 
 const App = () => {
 	return (
 		<div>
-			<div>
-				<h4>Enter an area of your choice</h4>
-			</div>
-			<div>
-				<form>
-					<h3>
-						Area: <input type="text" placeholder="Enter an area" />
-					</h3>
-					<div>
-						<button>Get Available Lots</button>
-					</div>
-					<div>
-						<button>Reset</button>
-					</div>
-				</form>
-			</div>
+			<Form />
+			<Clock />
 		</div>
 	);
 };
